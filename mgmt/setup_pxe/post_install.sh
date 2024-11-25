679c5354159962ff381b4d2468f02cb9a238b597
Michael Hohenstein <michael@hohenste.in>
2024-11-25 22:34:20 +0100

    mDNS finally working

--- mgmt/setup_pxe/post_install.sh
+++ mgmt/setup_pxe/post_install.sh
@@ -24,3 +24,28 @@ chmod 600 /home/praktikum/.ssh/authorized_keys
 
 # disable password authentication for ssh
 sed -i 's/^#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+
+# get the hostname provided by the dhcp server and inject it into the /etc/hosts file
+# Make a test request with dhcpcd and get the output
+DHCP_HOSTNAME=$(dhcpcd -T 2>/dev/null | grep "new_host_name=" | sed -n "s/.*='\([^']*\)'.*/\1/p")
+#DHCP_DOMAIN=$(dhcpcd -T | grep "new_domain_name=" | sed -n"s/.*='\([^']*\)'.*/\1/p")  # not used
+
+# check if there is a hostname provided by the dhcp server
+# if not, exit the script and print an error message
+if [[ -z "$DHCP_HOSTNAME" ]]; then
+    echo "No DHCP-assigned hostname found."
+    exit 1
+fi
+
+# for debugging purposes this information might be useful some time
+#echo "DHCP-assigned hostname: $DHCP_HOSTNAME"
+
+# Update /etc/hostname
+[ -f /etc/hostname ] && rm /etc/hostname
+echo "$DHCP_HOSTNAME" > /etc/hostname
+echo "Updated /etc/hostname with DHCP-assigned hostname: $DHCP_HOSTNAME"
+
+# Update the current hostname without reboot
+# this is not done but it might be useful to keep it
+#hostnamectl set-hostname "$DHCP_HOSTNAME"
+
