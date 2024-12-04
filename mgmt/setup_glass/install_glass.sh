7d884d7f660598dccb9c491f1360490002c9d68b
Michael Hohenstein <michael@hohenste.in>
2024-12-04 18:58:34 +0100

    configure glass-isc-dhcp web gui

---
+++ mgmt/setup_glass/install_glass.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+packages=(
+	# packages to build glass dependencies
+	# maybe important when installing it on a non-ix86 architecture
+	unzip
+	libtool
+	autoconf
+	automake
+	make
+	# package to run glass, not a dependency
+	nodejs
+)
+
+# install nodejs and nodesource
+# I don't know if i can install nodesource via the package manager or if i manually have to update the following link:
+curl -sL https://deb.nodesource.com/setup_23.x | bash -
+apt install -y ${packages[@]}
+
+# store cwd to return to it later
+current_dir=$(pwd)
+
+# install glass
+[ -d /opt/glass-isc-dhcp ] && rm -rf /opt/glass-isc-dhcp
+git clone https://github.com/Akkadius/glass-isc-dhcp.git /opt/glass-isc-dhcp
+cd /opt/glass-isc-dhcp
+mkdir logs
+chmod u+x ./bin/ -R
+chmod u+x *.sh
+
+npm install
+npm install forever -g
+
+# return to previous working directory
+cd ${current_dir}
+
+# specify admin username and password
+sed -i "s/\"admin_user\": .*,/\"admin_user\": \"root\",/" /opt/glass-isc-dhcp/config/glass_config.json
+# read password from secret file, only first line, remote newline and in the end shell-escape the string
+password=$(cat ./../../secrets/password.txt | head -n 1 | tr -d '\n' | sed -e 's/[&/]/\\&/g')
+sed -i "s/\"admin_password\": .*,/\"admin_password\": \"${password}\",/" /opt/glass-isc-dhcp/config/glass_config.json
+
+# the glasses server is not started. Instead, a systemd service unit should be created
+[ -f /etc/systemd/system/glass.service ] && rm /etc/systemd/system/glass.service
+cat << EOF > /etc/systemd/system/glass.service
+[Unit]
+Description=Glass ISC DHCP Server
+After=network.target
+
+[Service]
+WorkingDirectory=/opt/glass-isc-dhcp
+ExecStart=node ./bin/www
+User=root
+Group=root
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable glass
+systemctl start glass
